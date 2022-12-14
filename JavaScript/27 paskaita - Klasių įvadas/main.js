44641900dc2dfd6b89acea369b08dc228062598b
ligitaZA <ligitazaicevaite@gmail.com>
2022-12-14 19:30:33 +0200

    sukomentuota 1

--- JavaScript/27 paskaita - Klasių įvadas/main.js
+++ JavaScript/27 paskaita - Klasių įvadas/main.js
@@ -20,7 +20,7 @@ let asmenysBeKlasiu = [
     vardas: 'Ligita',
     amzius: 25,
     turiVaiku: false
-  },{
+  }, {
     vardas: 'Tadas',
     amzius: 29,
     turiVaiku: false
@@ -29,38 +29,38 @@ let asmenysBeKlasiu = [
 
 // kuriame objektų masyvą su klasėmis 
 {
-class Asmuo{
-  constructor(a,b,c){
-    this.vardas = a.split(' ')[0];  //nukerpa pavarde
-    this.pavarde = a.split(' ')[1]; // nukerpa varda
-    this.amzius = b;
-    this.turiVaiku = c;
+  class Asmuo {
+    constructor(a, b, c) {
+      this.vardas = a.split(' ')[0];  //nukerpa pavarde
+      this.pavarde = a.split(' ')[1]; // nukerpa varda
+      this.amzius = b;
+      this.turiVaiku = c;
+    }
+    pasisveikinaSu(vardas) {
+      return this.vardas + ' pasisveikina su ' + vardas; //metodas
+    }
   }
-  pasisveikinaSu(vardas){
-    return this.vardas + ' pasisveikina su ' + vardas; //metodas
-  }
-}
-let asmenysSuKlasemis = [
-  new Asmuo('Lukas Lukauskas', 10, false),
-  new Asmuo('Joana Kleinauske', 56, true)
-];
-console.log(asmenysSuKlasemis)
-console.log(asmenysSuKlasemis[0].pasisveikinaSu('Jonas'))
-// // let zmogus = new Asmuo('Vardas', 99, false);
-// console.log(zmogus);
+  let asmenysSuKlasemis = [
+    new Asmuo('Lukas Lukauskas', 10, false),
+    new Asmuo('Joana Kleinauske', 56, true)
+  ];
+  console.log(asmenysSuKlasemis)
+  console.log(asmenysSuKlasemis[0].pasisveikinaSu('Jonas'))
+  // // let zmogus = new Asmuo('Vardas', 99, false);
+  // console.log(zmogus);
 };
 
 // task
 
 
 class Ledai {
-  constructor(pavadinimas,skonis,kaina){
+  constructor(pavadinimas, skonis, kaina) {
     this.pavadinimas = pavadinimas;
     this.skonis = skonis;
     this.kaina = kaina;
   }
-  kainosKeitimas(keitimasProcentais){ //kai nori pakeisti kaina (88 eilute), galima rasyti ir kitoki procenta kiek nori sumazint, padidint kaina
-    return this.kaina + this.kaina * keitimasProcentais/100
+  kainosKeitimas(keitimasProcentais) { //kai nori pakeisti kaina (88 eilute), galima rasyti ir kitoki procenta kiek nori sumazint, padidint kaina
+    return this.kaina + this.kaina * keitimasProcentais / 100
     //0.60 + 0.60 * -37/100
   }
 }
@@ -100,19 +100,20 @@ console.groupEnd();
 //                Užduotys
 //    TASK 1
 
+
 console.groupCollapsed('TASK 1')
 
 //1) Sukurkite klasę vardu "Car" ir duokite jai "name" ir "year" parametrus. +
 //1.1) Sukurkite papildomą parametrą "helloCar" ir priskirkite jam tekstą, pristatantį mašiną. (gali būti betkas, tarkim "Labas, čia mano mašina Volvo ir ji buvo pagaminta 1990 metais"). +
- //1.2) Skukurkite metodą "age", kuris apskaičiuotų ir grąžintų tekstą, pasakantį kiek mašinai metų. (dabartinę datą gauti galima su 'date = new Date()' ir paversti tik į metus su 'date.getFullYear()'). +
-  
+//1.2) Skukurkite metodą "age", kuris apskaičiuotų ir grąžintų tekstą, pasakantį kiek mašinai metų. (dabartinę datą gauti galima su 'date = new Date()' ir paversti tik į metus su 'date.getFullYear()'). +
+
 class Car {
-  constructor(name, year){
+  constructor(name, year) {
     this.name = name;
     this.year = year;
-    this.helloCar = `Labas, šiuo metu vairuoju ' + ${this.name} mašina buvo pagaminta ${this.year} metais.`;
+    this.helloCar = `Šiuo metu vairuoju ${this.name}, mašina buvo pagaminta ${this.year} metais.`;
   }
-//metodas
+  //metodas
   age() {
     let dabartineData = new Date();
     dabartineData = dabartineData.getFullYear();
@@ -130,6 +131,8 @@ masinos.forEach(masina => console.log(masina.age()));
 console.groupEnd();
 
 
+
+
 //        TASK 2
 
 console.groupCollapsed('TASK 2')
@@ -139,24 +142,24 @@ console.groupCollapsed('TASK 2')
 //   2.3) Sukurkite metodą "perimeter", kuris grąžina stačiakampio perimetrą. +
 
 class Rectangle {
-  constructor(width, height){
+  constructor(width, height) {
     this.width = width;
     this.height = height;
-    this.color = `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`;
+    this.color = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`; // tas pats - Math.floor(Math.random()*16777215).toString(16)
   }
-  area (){
-    let staciakampioPlotas = this.width* this.height;
+  area() {
+    let staciakampioPlotas = this.width * this.height;
     return staciakampioPlotas;
   }
-  perimeter (){
-    let staciakampioPerimetras =  this.width*2 + this.height*2;
+  perimeter() {
+    let staciakampioPerimetras = this.width * 2 + this.height * 2;
     return staciakampioPerimetras;
   }
 }
-let staciakampiai =[
-  new Rectangle(5,10),
-  new Rectangle(2,8),
-  new Rectangle(3,8)
+let staciakampiai = [
+  new Rectangle(5, 10),
+  new Rectangle(2, 8),
+  new Rectangle(3, 8)
 ];
 
 // pasitikrinam ar veikia area ir perimeter
@@ -165,6 +168,8 @@ staciakampiai.forEach(staciakampis => console.log(staciakampis.area()));
 staciakampiai.forEach(staciakampis => console.log(staciakampis.perimeter()));
 
 console.groupEnd();
+
+
 
 //          TASK 3
 
@@ -175,23 +180,23 @@ console.groupCollapsed('TASK 3');
 //   3.2) Sukurkite metodą "distance", kuris grąžina atstumą tarp dviejų taškų (Point'ų). (siūlau naudoti Math.hypot() metodą). +
 
 class Point {
-  constructor(x,y){
+  constructor(x, y) {
     this.x = x;
     this.y = y;
   }
-  coordinates(){
+  coordinates() {
     return `(${this.x}, ${this.y})`;
   }
-  distance(taskas){
-    return `Atstumas tarp  dviejų taškų yra: ${Math.hypot(this.x-taskas.x,this.y-taskas.y)}`;
+  distance(taskas) {
+    return `Atstumas tarp  dviejų taškų yra: ${Math.hypot(this.x - taskas.x, this.y - taskas.y)}`;
     //this.x-taskas.x pirma pakeliami kvadratu abu skaiciai, viena is kito atima, tada sudeda su this.y-taskas.y ir is ju sumos traukia sakni 
   }
 }
-let taskas1 = new Point (3,12);
-let taskas2 = new Point (5,9);
-let taskas3 = new Point (2,8);
-let taskas4 = new Point (23,2);
-let taskas5 = new Point (1,21);
+let taskas1 = new Point(3, 12);
+let taskas2 = new Point(5, 9);
+let taskas3 = new Point(2, 8);
+let taskas4 = new Point(23, 2);
+let taskas5 = new Point(1, 21);
 console.log(taskas1.distance(taskas2));
 console.log(taskas3.distance(taskas5));
 // ima taskas3 ir taskas5 ir skaiciuoja atstuma tarp ju su math.hypot (186 eilute)
@@ -199,41 +204,124 @@ console.log(taskas3.distance(taskas5));
 console.groupEnd();
 
 
+
+
 //          TASK 4
 
 console.groupCollapsed('TASK 4');
 
 //  4) Sukurkite klasę vardu "Rectangle_3D" ir duokite jai "width", "height" ir "depth" parametrus. +
-  // 4.1) Sukurkite metodą "volume", kuris grąžina 3D stačiakampio tūrį.+
-  // 4.2) Sukurkite metodą "surfaceArea", kuris grąžina 3D stačiakampio paviršiaus plotą.+
-  // 4.3) Sukurkite metodą "perimeter", kuris grąžina 3D stačiakampio perimetrą.
+// 4.1) Sukurkite metodą "volume", kuris grąžina 3D stačiakampio tūrį.+
+// 4.2) Sukurkite metodą "surfaceArea", kuris grąžina 3D stačiakampio paviršiaus plotą.+
+// 4.3) Sukurkite metodą "perimeter", kuris grąžina 3D stačiakampio perimetrą.
 
-  class Rectangle_3D{
-    constructor(width, height, depth){
-      this.width = width;
-      this.height = height;
-      this.depth = depth;
-    }
-    volume(){
-      let turis = this.width*this.height*this.depth;
-      return turis;
-    }
-    surfaceArea(){
-      let pavirsiausPlotas = 2*((this.width*this.height)+(this.width*this.depth)+(this.height*this.depth));
-      return pavirsiausPlotas;
-      //S=2(ab+ac+bc)
-    }
-    perimeter(){
-      let perimetras = 4*(this.width+this.height+this.depth);
-      return perimetras;
-      //Perimeter of a Square, (P) = 4 × Side units.
-    }
+class Rectangle_3D {
+  constructor(width, height, depth) {
+    this.width = width;
+    this.height = height;
+    this.depth = depth;
   }
-    let kvadratas = new Rectangle_3D(2,2,4);
+  volume() {
+    let turis = this.width * this.height * this.depth;
+    return turis;
+  }
+  surfaceArea() {
+    let pavirsiausPlotas = 2 * ((this.width * this.height) + (this.width * this.depth) + (this.height * this.depth));
+    return pavirsiausPlotas;
+    //S=2(ab+ac+bc)
+  }
+  perimeter() {
+    let perimetras = 4 * (this.width + this.height + this.depth);
+    return perimetras;
+    //Perimeter of a Square, (P) = 4 × Side units.
+  }
+}
+let kvadratas = new Rectangle_3D(5, 2, 4);
 
-  // pasitikrinam
-    console.log(kvadratas.volume());
-    console.log(kvadratas.surfaceArea());
-    console.log(kvadratas.perimeter());
+// pasitikrinam
+console.log(kvadratas.volume());
+console.log(kvadratas.surfaceArea());
+console.log(kvadratas.perimeter());
+
+console.groupEnd();
+
+
+
+//          TASK 5
+// 5+) Pabandyti sukurti UI (User Interface (Vartotojo Sąsaja)) 1-4 užduotims ir spausdinti informaciją į ekraną. (Kas nori pasižiūrėkite JS Canvas ir su juo pabandykite atvaizduoti figūras puslapyje(bent jau 2D))
+console.groupCollapsed('TASK 5')
+
+document.querySelector("#task1")
+  .addEventListener("submit", e => {
+    e.preventDefault();
+    let modelis = e.target.elements.pavadinimas.value;
+    let metai = e.target.elements.pagaminta.value;
+    let masina = new Car(modelis, metai);
+    document.querySelector("#automobiliai > div").innerHTML += `
+    <div>
+      <h1> Automobilis:  ${masina.name} <br> Metai: ${masina.year} </h1>
+      <p> ${masina.helloCar} </p>
+      <p> Jūsų automobilis ${masina.name} yra ${masina.age()} metų amžiaus.</p>
+    </div>
+  `;
+    e.target.elements.pavadinimas.value = null;
+    e.target.elements.pagaminta.value = null;
+  });
+
+
+document.querySelector('#task2').addEventListener('submit', e => {
+  e.preventDefault();
+  let plotis = e.target.elements.width.value;
+  let aukstis = e.target.elements.height.value;
+  let staciakampis = new Rectangle(plotis, aukstis);
+  document.querySelector('#rectangle > div').innerHTML += `
+  <div>
+  <h2>Plotas: ${staciakampis.area()}</h2>
+  <h2>Perimetras:${staciakampis.perimeter()}</h2>
+  </div>
+  <h2>Stačiakampis pagal pateiktą plotį ir aukštį (px):`;
+  
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+     ctx.fillStyle = `${staciakampis.color}`;
+  ctx.fillRect(10, 10, `${plotis}`,`${aukstis}` );
+ 
+  e.target.elements.width.value = null;
+  e.target.elements.height.value = null;
+
+});
+
+console.groupEnd();
+
+
+
+
+
+// // Canvas
+console.groupCollapsed('Canvas')
+// var c = document.getElementById("myCanvas");
+// var ctx = c.getContext("2d");
+
+// // Create gradient
+// var grd = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
+// grd.addColorStop(0, "red");
+// grd.addColorStop(1, "white");
+
+// // Fill with gradient
+// ctx.fillStyle = grd;
+// ctx.fillRect(10, 10, 150, 80);
+
+// const canvas = document.getElementById("canvas");
+// const ct = canvas.getContext("2d");
+
+// for (let i = 0; i < 6; i++) {
+//   for (let j = 0; j < 6; j++) {
+//     ct.fillStyle = `rgb(
+//         ${Math.floor(255 - 42.5 * i)},
+//         ${Math.floor(255 - 42.5 * j)},
+//         0)`;
+//     ct.fillRect(j * 25, i * 25, 25, 25);
+//   }
+// }
 
 console.groupEnd();
---
+++ JavaScript/28 paskaita - OOP principai/main.js
@@ -0,0 +1,284 @@
+//      OOP Principai
+
+/*
+Youtube:
+
+TraversyMedia (gera info);
+
+Programming with mosh;
+The net ninja(geri tutorialai);
+WebDev; (me tik js)
+Free code camp; (daug info)
+The coding train (naujienos ir projektai,  pradedančiąjam nelabai);
+FireShip ();
+CodeBullet (for fun);
+KevinPowell (html ir css);
+DevDreamer (trumpi, aiškūs video);
+
+
+*/
+
+
+/*
+Inkapsiuliacija       - objekto vidiniia duomenys slepiamiir jais galima manipuliuoti tik naudojant objekto viešus metodus.
+
+Abstrakcija           - objekto 
+nepriklausomumas nuo išorinnių sudedamųjų dalių.
+
+Paveldėjimas          - viena klasė gali būti kitos klasės konkretizacija.
+
+Polimorfizmas         - paveldi, bet kažkiek modifikuoja, prideda naujų.
+*/
+
+
+class Gyvunas {
+  #pavadinimas; // privatus
+  #kojuKiekis
+  constructor(pavadinimas, kojuKiekis) {
+    this.pavadinimas = pavadinimas;
+    this.kojuKiekis = kojuKiekis;
+  }
+  getPavadinimas() {
+    return this.#pavadinimas; //paimi pavadinimas
+  }
+  setPavadinimas(naujasPavadinimas) {
+    this.#pavadinimas = naujasPavadinimas; //pakeiti pavadinima
+  }
+  getKojuKiekis() {
+    return this.#kojuKiekis;
+  }
+  setKojuKiekis(naujasKojuKiekis) {
+    return this.#kojuKiekis = naujasKojuKiekis;
+  }
+  valgo() {
+    return 'kazka'
+  }
+}
+
+class Kate extends Gyvunas {   // klase kate gauna visus gyvunas nustatymus, negauna tik konstruktoriaus ir jo nustatymu
+  constructor(kojuKiekis) {
+    super('kate', kojuKiekis); //idedam gyvunas construktoriu su sitais nustatymais
+  }
+  #balsoVariantai = ['miau', 'mur', 'prrrrrru', 'sssssshhhhhh']
+  #balsoVariantas() {
+    return this.#balsoVariantai[Math.floor(Math.random() * this.#balsoVariantai.length)];
+  }
+  balsas() {
+    return this.#balsoVariantas();
+  }
+  valgo() {
+    return 'peles'
+  }
+}
+class Voras extends Gyvunas {
+  constructor(kojuKiekis) {
+    super('Voras', kojuKiekis);
+  }
+  gasdina() {
+    return 'bu'
+  }
+  valgo() {
+    return 'muses'
+  }
+}
+
+let gyvunas = [
+  new Gyvunas('Kate', 4),
+  new Kate(4),
+  new Voras(8)
+]
+
+
+//1) Sukurkite su HTML formą su vienu laukeliu - fullname, ir po apačia - lentelę su dviem stulpeliais - name ir surname.
+//1.1) JavaScripte pasirašykite klasę su konstruktoriumi, kuris turės vardą ir pavardę. Metodą - atsirasti lentelėje.
+//1.2) Kai vartotojas įrašo savo pilną vardą - su JS metodais išskirkite jį į dvi dalis; pasirūpinkite, kad nebūtų tarpelių prieš ir po vardo; pirmą raidę vardo ir pavardės padidinkit, o kitas - sumažinkit (capitalization).
+//1.3) Sukurkite objektą naudojant Klasę.
+//1.4) Galiausiai iškvieskite metodą, kad pridėtų į lentelę.
+//Taip, naudojant OOP pagrindus, vartotojui įrašius duomenis į formą, atsiras apačioje esančioje lentelėje išskirtas vardas ir pavardė, o ir leis tolimesniai projekto plėtrai (t.y. darbui su objektais).
+
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = this.getFormattedName(firstName);//vardas=sutvarkyto fullname vardui 
+    this.lastName = this.getFormattedName(lastName);//pavarde=sutvarkyto fullname pavardei
+  }
+  getFormattedName(name) {
+    return name[0].toLocaleUpperCase() + name.slice(1).toLocaleLowerCase(); //vardas ir pavarde grazinami, 0-oji raide verciama i didziaja o 1-oji nukerpama(nes liko), likes tekstas verciamas mazosios raidem
+  }
+
+  addToTheTable() { // idedu varda i table
+    const usersListElement = document.querySelector("tbody#users-list"); //obvious
+    const nameRow = document.createElement("tr");//kuriu table row
+    const firstNameElement = document.createElement("td"); // kuriu table data kuriame bus mano name
+    const lastNameElement = document.createElement("td");// kuriu table data kuriame bus mano last name
+
+    firstNameElement.textContent = this.firstName; //i susikurta firstnameelement dedu savo firstname
+    lastNameElement.textContent = this.lastName;// same tik kad dedu savo lastname
+
+    nameRow.append(firstNameElement, lastNameElement); //appendinu abu
+
+    usersListElement.append(nameRow); // appendinu visa table row
+  }
+}
+const fullNameEntryForm = document.querySelector("form#fullNameEntry");//pasirenku forma, kur vartotojas ves savo duomenis
+
+fullNameEntryForm.addEventListener("submit", (event) => { //dedu event listeneri submitui
+  event.preventDefault();
+
+  const fullName = document.querySelector("#fullNameInput").value.trim(); // is vartotojo ivesto inputo paimu value ir triminu (iskerpu white space)
+
+  const firstName = fullName.split(" ")[0]; //splitinu ties tarpeliu pirma varda
+  const lastName = fullName.slice(firstName.length).trim(); // slicinu first name ir triminu white space
+
+  const user = new Person(firstName, lastName); //user prilyginamas naujam person su firstname ir lastname
+
+  user.addToTheTable(); //user pridedamas i lentele
+
+  // console.log({ user }); //consolinam
+});
+
+// 2) Sukurkite HTML formą, kurioje vartotojas galės įrašyti (į input laukelius): car brand, model, mileage, price ir image (url laukelis).
+//   2.1) Per Klasę, sukuriamas objektas ir jis atvaizduojamas po forma (CSS'ą rašykite CSS'e) kaip atvaizduota nuotraukoje (./images/cars.PNG).
+//   3.1) Paspaudus ant automobilio bloko - turi alert išmesti kainą ir mileage. (gal su CSS'u gražiai padaryti, kad užėjus ant elemento su pele, atsiranda laukelis nuotraukoje...)
+class Car {
+  constructor (brand, model, mileage, price, image) {
+    this.brand = brand;
+    this.model = model;
+    this.mileage = mileage;
+    this.price = price;
+    this.image = image;
+  }
+  post() {
+    const container = document.querySelector('.container');
+    const createDiv = document.createElement('div');
+    createDiv.className = '.card';
+    const createImage = document.createElement('img');
+    createImage.src = this.image;
+    const createText = document.createElement('h2');
+    createText.innerText = `${this.brand} ${this.model}`;
+    createDiv.append(createImage, createText);
+    container.append(createDiv);
+    createDiv.addEventListener('click', () => {
+      const createP = document.createElement('p');
+      createP.innerText = `${this.price}`;
+      createDiv.append(createP);
+      console.log(this.price);
+    })
+  }
+}
+
+document.querySelector('#carForm').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const brand = document.querySelector('#brand').value;
+  const model = document.querySelector('#model').value;
+  const mileage = document.querySelector('#mileage').value;
+  const price = document.querySelector('#price').value;
+  const image = document.querySelector('#image').value;
+  const car = new Car(brand, model, mileage, price, image);
+  car.post();
+})
+
+
+// 3+) (PVP battle) https://edabit.com/challenge/ifDM26p25bqS8EsFu
+// 4+) (Shiritori) https://edabit.com/challenge/6o5tYwwbY2ys7XTm4
+
+
+// Susikurti (h1-h6) Antraštės Klasę su tekstu, atributais
+class Heading{
+  constructor(props){
+    this.props = props;
+    return this.render();
+  }
+
+  render = () =>{
+    this.htmlElement = document.createElement(`h${this.props.dydis}`);
+    this.textElement = document.createTextNode(this.props.tekstas);   
+    this.htmlElement.append(this.textElement);
+
+    if(this.props.atributai){
+      // jeigu atributai yra objektas
+      Object.keys(this.props.atributai).forEach(raktas => {
+        this.htmlElement.setAttribute(raktas, this.props.atributai[raktas]);
+      });
+
+      // jeigu atributai yra masyvai masyve
+      // this.props.atributai.forEach(atributas => {
+      //   this.htmlElement.setAttribute(atributas[0], atributas[1]);
+      // });
+    }
+
+    return this.htmlElement;
+  }
+}
+
+let antraste1 = new Heading({
+  dydis: '1',
+  tekstas: 'Kaimietiškai',
+  atributai: {
+    class: 'klasesVardas darVienaKlase',
+    id: 'kazkoksId',
+    style: 'color:red'
+  }
+  // atributai: [ 
+  //   ['class', 'klasesVardas darVienaKlase'],
+  //   ['id', 'kazkoksId'],
+  //   ['style', 'color:red']
+  // ]
+});
+document.querySelector("body").append(antraste1);
+document.querySelector("body").append(new Heading({
+  dydis:'5',
+  tekstas:'Labas rytas',
+  // atributai: [
+  //   ['style', 'font-size: 50px']
+  // ]
+  atributai: {
+    style: 'font-size:50px'
+  }
+}));
+document.querySelector("body").append(new Heading({
+  dydis:'6',
+  tekstas:'mažiukas'
+}));
+
+//5) Sukurti Klasę, kuri kurs paragrafus.
+
+class Paragraph {
+  constructor(props) {
+    this.props = props;
+    return this.render();
+  }
+  render = () => {
+    this.htmlElement = document.createElement('p');
+    this.textElement = document.createTextNode(this.props.tekstas);
+    this.htmlElement.append(this.textElement);
+    if (this.props.atributai) {
+      Object.keys(this.props.atributai).forEach(raktas => {
+        this.htmlElement.setAttribute(raktas, this.props.atributai[raktas]);
+      });
+    }
+    return this.htmlElement;
+  }
+}
+let paragrafas1 = new Paragraph({
+  tekstas:
+    "Lorem ipsum dolor sit ame  consectetur adipisicing elit. Quisquam, quod.",
+  atributai: {
+    class: "klasesVardas darVienaKlase",
+    id: "kazkoksId",
+    style: "color:green"
+  }
+});
+document.querySelector("body").append(paragrafas1);
+document.querySelector("body").append(
+  new Paragraph({
+    tekstas: "Laba diena",
+    atributai: {
+      style: "font-size:20px"
+    }
+  })
+);
+document.querySelector("body").append(
+  new Paragraph({
+    tekstas: "Paragrafas"
+  })
+);
